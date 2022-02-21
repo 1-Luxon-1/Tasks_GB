8a4d80b00d90809ffa1e445978c35f3cc11ab34e
Luxon <andrey-juravlev2013@yandex.ru>
2022-02-22 02:15:35 +0300

    add Task7

---
+++ Task7/mylib.h
@@ -0,0 +1,7 @@
+#pragma once
+namespace Mylib
+{
+	void fillArr(float arr[], const size_t size);
+	void printArr(const float arr[], const size_t size);
+	void countPositiveNegative(const float arr[], const size_t size);
+}
