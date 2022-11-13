2249f29f7c3b8b69ffd5a6818b52f43a0929a50d
Luxon <andrey-juravlev2013@yandex.ru>
2022-11-13 19:39:36 +0300

    add task5 and task6

---
+++ 6_Hard_moment_C++/lesson5/Task_5.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <array>
+#include <vector>
+#include <deque>
+#include <unordered_map>
+#include <map>
+#include <string>
+using namespace std;
+
+///////////////////////////// Task 1 /////////////////////////////
+template<typename T>
+void countigWords(T beg, T en)
+{
+	unordered_map<string, int>m;
+	for (auto it = beg; it < en; it++)
+	{
+		++m[*it];
+	}
+
+	vector<pair<string, int>>words(m.begin(), m.end());
+	for (auto i : words)
+	{
+		if (i.second == 1)
+		{
+			cout << i.first << " ";
+		}
+	}
+}
+//////////////////////////////////////////////////////////////////
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	///////////////////////////// Task 1 ////////////////////////////
+	array<string, 10>arrWords{ "array", "running", "keys", "keys", "values", "specific", "operator", "running", "array", "operator" };
+	vector<string>vecWords{ "sleep", "running", "sleep", "running" };
+	deque<string>decWords{ "coding", "checks", "read", "programm", "coding", "checks" };
+	countigWords<array<string, 10>::iterator>(arrWords.begin(), arrWords.end());
+	cout << endl;
+	countigWords<vector<string>::iterator>(vecWords.begin(), vecWords.end());
+	cout << endl;
+	countigWords<deque<string>::iterator>(decWords.begin(), decWords.end());
+	//////////////////////////////////////////////////////////////////
+
+	///////////////////////////// Task 2 ////////////////////////////
+	string str;
+	cout << endl << "Введите текст: " << endl;
+	getline(cin, str);
+	cout << endl << "Вы ввели:" << endl;
+	cout << str;
+	multimap<int, string>mp;
+	int num = 0;
+	string tempstr;
+	int begin = 0;
+	int end = 0;
+	for (auto it : str)
+	{
+		end++;
+		if (it == '.' || it == '?' || it == '!' || it+1 == string::npos)
+		{
+			tempstr.clear();
+			copy(str.begin() + begin, str.begin() + end, back_inserter(tempstr));
+			num = count_if(str.begin() + begin, str.begin() + end, [](char c) { return (c == ' '); });
+			mp.insert({ ++num,tempstr });
+			begin = end + 1;
+		}
+	}
+	cout << endl << endl;
+	for (auto it : mp)
+	{
+		cout << "Количество слов: " << it.first << ":  " << it.second << endl;
+	}
+
+return 0;
+}
---
+++ 6_Hard_moment_C++/lesson6/Task_6.cpp
@@ -0,0 +1,158 @@
+﻿#include <iostream>
+#include <chrono>
+#include <thread>
+#include <mutex>
+#include <vector>
+#include <time.h>
+#include <algorithm>
+using namespace std;
+
+/////////////////////// Task 1 ///////////////////////
+mutex m;
+void funcA()
+{
+	cout << __FUNCTION__ << endl;
+}
+
+void funcB()
+{
+	cout << __FUNCTION__ << endl;
+}
+//////////////////////////////////////////////////////
+
+class pcount
+{
+public:
+	friend	ostream& operator<<(ostream&, pcount&);
+	pcount(thread& th)
+	{
+		t = &th;
+	}
+	void select(thread& th)
+	{
+		t = &th;
+	}
+private:
+	thread* t;
+};
+ostream& operator<<(ostream& out, pcount& p)
+{
+	lock_guard mut(m);
+	out << __FUNCTION__ << endl;
+	out << p.t->get_id() << endl
+		<< p.t->hardware_concurrency << endl;
+	return out;
+}
+//////////////////////////////////////////////////////
+
+
+/////////////////////// Task 2 ///////////////////////
+int searchSimpleNumber(int i)
+{
+	int count = 1;
+	int res = 2;
+	for (int it = 3; count < i; it += 2)
+	{
+		for (int j = 2; j < it; j++)
+		{
+			if (it % j == 0)
+				break;
+			else if (j + 1 == it)
+			{
+				count++;
+				res = it;
+				cout << "Progress: " << ((double)count / i) * 100 << " %" << endl;
+			}
+		}
+	}
+	return res;
+}
+//////////////////////////////////////////////////////
+
+
+/////////////////////// Task 3 ///////////////////////
+void printVec(vector<int>& v)
+{
+	for (auto it : v)
+	{
+		cout << it << "  ";
+	}
+	cout << endl;
+}
+
+void addThing(vector<int> &v)
+{
+	while (v.size() != 0)
+	{
+		int i = 0;
+		this_thread::sleep_for(1000ms);
+		if (v.size() == 0)
+			return;
+		v.push_back(rand() % 1000);
+		i++;
+		cout << "Принёс: ";
+		printVec(v);
+	}
+}
+
+void theft(vector<int> &v)
+{
+	while (v.size() != 0)
+	{
+
+		int i = 0;
+		this_thread::sleep_for(500ms);
+		int max = v[0];
+		for (auto it : v)
+		{
+			if (it > max)
+			{
+				max = it;
+			}
+			i++;
+		}
+		sort(v.begin(), v.end());
+		v.pop_back();
+		cout << "Украл: ";
+		printVec(v);
+	}
+}
+//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+
+
+int main()
+{
+	srand(time(NULL));
+	setlocale(LC_ALL, "RUS");
+	/////////////////////// Task 1 ///////////////////////
+	thread th1(funcA);
+	thread th2(funcB);
+	pcount pc(th1);
+	cout << pc << endl;
+	pc.select(th2);
+	cout << pc << endl;
+	th1.join();
+	th2.join();
+	/////////////////////// Task 2 ///////////////////////
+
+	int simpleNumber;
+	int i;
+	cout << "Введите номер простого числа: ";
+	cin >> i;
+	thread t2([&]() {simpleNumber = searchSimpleNumber(i); });
+
+	cout << i << "-е простое число = " << simpleNumber << endl;
+
+
+	/////////////////////// Task 3 ///////////////////////
+	vector<int>things{ 2, 45, 213, 87, 875, 127, 753, 1111 };
+	thread owner(addThing, ref(things));
+	thread theif(theft, ref(things));
+	//////////////////////////////////////////////////////
+
+	t2.join();
+	owner.join();
+	theif.join();
+return 0;
+}
